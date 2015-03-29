1ad6074ebd756ce7f35829df3589df85c429786c
huangyan <345471536@qq.com>
2015-03-30 00:57:28 +0800

    '部署简化viewstest4'

--- hehotel/views.py
+++ hehotel/views.py
@@ -68,6 +68,6 @@ def site_update(request):
     command = '/tor.sh > /tor.log'
     log = logging.getLogger('log')
     timestr = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    log.info('run command2:' + timestr)
+    log.info('run command3:' + timestr)
     subprocess.call(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     return render_to_response('main/site_update.html', RequestContext(request, locals()))
