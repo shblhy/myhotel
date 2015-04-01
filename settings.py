ef80dfccca4e0e16712d00eb5549ef184e1f342f
huangyan <345471536@qq.com>
2015-04-02 00:57:16 +0800

    '更改线上数据库'

--- settings.py
+++ settings.py
@@ -72,21 +72,22 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hehotel',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '9729e316fb01',                  # Not used with sqlite3.
-        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '36114'                    # Set to empty string for default. Not used with sqlite3.
-        },
-    #'easy':{
-    #   'ENGINE': 'django.db.backends.sqlite3',
-    #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #},
+       'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(os.path.join(BASE_DIR, 'data'), 'db.sqlite3'),
+    },
 }
+#DATABASES = { #线上数据库要用于别的服务，性能波动大，所以暂且都用sqlite3吧
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'hehotel',                      # Or path to database file if using sqlite3.
+#        'USER': 'root',                      # Not used with sqlite3.
+#        'PASSWORD': '9729e316fb01',                  # Not used with sqlite3.
+#        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '36114'                    # Set to empty string for default. Not used with sqlite3.
+#        },
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
